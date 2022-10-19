eb98b4063437633f57ba5acf0701ee3bc4905a07
Vojtěch Gistr <vojtech.gistr@email.cz>
2022-10-19 22:34:23 +0200

    Update example.h

--- project_files/include/example.h
+++ project_files/include/example.h
@@ -1,8 +1,8 @@
-#ifndef GDEXAMPLE_H
-#define GDEXAMPLE_H
+#ifndef GDEXAMPLE_H         // include guard
+#define GDEXAMPLE_H 
 
-#include <Godot.hpp>
-#include <Sprite.hpp>
+#include <Godot.hpp>        // Godot API
+#include <Sprite.hpp>       // Sprite class
 
 namespace godot
 {
@@ -10,25 +10,30 @@ namespace godot
     //   class name : inheritance
     class GDExample : public Sprite
     {
+        //       class name, inheritance
         GODOT_CLASS(GDExample, Sprite)
 
     private:
-        float time_passed;
-        godot::Godot *gd;
+        float time_passed;      // variable to store time passed since last frame
+        godot::Godot *gd;       // Pointer to Godot API
+
 
     public:
+        //  Here you register all your methods you want to use in Godot Engine
         static void _register_methods();
 
-        GDExample();
-        ~GDExample();
+        GDExample();                    // constructor
 
-        void _init(); // our initializer called by Godot
+        ~GDExample();                   // destructor
 
-        void _ready();
+        void _init();                   // called when the object is instantiated
 
-        void _process(float delta);
+        void _ready();                  // called when the node enters the scene tree
+
+        void _process(float delta);     // called every frame
 
     protected:
+
     };
 
 }
